49cc0789c209e3851f8670b3dcf9e9b0a2c03aef
cadmath <48750174+cadmath@users.noreply.github.com>
2019-03-21 22:48:26 +0300

    Add files via upload

---
+++ rec.php
@@ -0,0 +1,13 @@
+﻿<html>
+<body>
+<form method="POST"> 
+Name: <input type="text" name="name" value="<?php echo $_POST['name'];?>">
+Value: <input type="text" name="value" value="<?php echo $_POST['value'];?>">
+<input type="submit" value="Отправить">
+</form>
+
+<?php
+echo $_POST['name'].' : '. $_POST['value'];
+?>
+</body>
+</html>
